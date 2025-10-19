4d038aff53d028006e1c7fdce09e6dfd9cda61ac
A.A.M.S <aliaikmohammedshameem@gmail.com>
2025-10-19 21:44:17 +0500

    Initial commit with version 1.0

---
+++ main.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+root = tk.Tk() # Declare the root object
+
+def process():
+    output = ['' for _ in range(10)]
+    count = 0
+    num_subjects = 0
+    total_mark = 0
+    for element in entries.subjects:
+        output[count] = float(entries.subjects[element].get())
+        if output[count] != -1.0:
+            num_subjects += 1
+            total_mark += output[count]
+        count += 1
+    agregate = 0
+    if num_subjects != 0:
+        agregate = (total_mark / (100 * num_subjects)) * 100
+    
+    print(output)
+    print(agregate)
+    aggregate_marks['entry'].delete(0, 'end')
+    aggregate_marks['entry'].insert(0, agregate)
+
+
+root.title('Marks aggregator v1.0') # makes the title of the window
+Window_Title = ttk.Label(root, text='Marks aggregator') # Main heading of the window
+
+class entries: # class containng entries for subjects where use inputs data
+    subjects = {
+        'english' : ttk.Entry(root),
+        'dhivehi' : ttk.Entry(root),
+        'mathematics' : ttk.Entry(root),
+        'chemistry' : ttk.Entry(root),
+        'physics' : ttk.Entry(root),
+        'biology' : ttk.Entry(root),
+        'marine science' : ttk.Entry(root),
+        'business' : ttk.Entry(root),
+        'accounting' : ttk.Entry(root),
+        'economics' : ttk.Entry(root)
+    }
+
+class labels: # Class contaning labels for subjects
+    subjects = {
+        'english' : ttk.Label(root, text='English'),
+        'dhivehi' : ttk.Label(root, text='Dhivehi'),
+        'mathematics' : ttk.Label(root, text='Mathematics'),
+        'chemistry' : ttk.Label(root, text='Chemistry'),
+        'physics' : ttk.Label(root, text='Physics'),
+        'biology' : ttk.Label(root, text='Biology'),
+        'marine science' : ttk.Label(root, text='Marine Science'),
+        'business' : ttk.Label(root, text='Business'),
+        'accounting' : ttk.Label(root, text='Accounting'),
+        'economics' : ttk.Label(root, text='Economics')
+    }
+
+aggregate_marks = { # Dictionary containing users agregate marks and its label
+    'label' : ttk.Label(root, text='Aggregated Mark'),
+    'entry' : ttk.Entry(root)
+}
+
+#aggregate_marks['entry'].config(state='readonly') # Sets aggregatemarks to read only
+aggregate_marks['entry'].insert(0, '0') # Sets agregate marks entry value to 0
+
+# sets the positions and arrangement of the entries, and labels
+row_count = 1
+for element in labels.subjects:
+    entries.subjects[element].insert(0, '-1')
+    entries.subjects[element].grid(row=row_count, column=1, padx=10, pady=5)
+    labels.subjects[element].grid(row=row_count, column=0, padx=10, pady=5)
+    row_count += 1
+column_count = 0
+for element in aggregate_marks:
+    aggregate_marks[element].grid(row=row_count+1, column=column_count, padx=10, pady=5)
+    column_count += 1
+Window_Title.grid(row=0, column=0, columnspan=2)
+
+# sets up the one button
+calculate = ttk.Button(root, text='Calculate', command=process)
+calculate.grid(row=row_count, column=0, columnspan=2, padx=5, pady=10)
+root.mainloop()
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Marks aggregator
+
+Made to be used to calculate marks of subjects and find the average mark of all the subjects
+
+Basic program made in python using tkinter and ttk libraries
+
+Made as a **basic coding project for fun during break times of mine**
+
+## Issues
+- Code base is very unclean with many typos and shit
+- the output list is init with str when float is being inserted into it
+- More sutable variable names should be found and used
+- code should be more structured
+
+## compiled binaries
+Not avalable yet, this isnt a final product
+
+you should be able to **compile** it using pyinstaller tho to make an executable binary
+
+No, i didnt make it os specific i think
+
+## dependencies
+- **Windows**
+    - Python 3.13 or 3.12 (whatever, it should work with any 3x version, i havnt tested)
+    > Note: Tkinter comes preinstalled with windows versions of python
+- **Linux**
+    - Fedora
+        - Install tkinter using `sudo dnf install python3-tkinter`
+    - Arch
+        - install tknter using `sudo pacman -S tk`
+    - ubuntu
+        - install tkinter using `sudo apt update && sudo apt install python3-tk`
+- **MacOS**
+    - i dont know, i dont own a mac
+
+
