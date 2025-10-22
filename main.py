f614de747031053cb38f30fabb1bbae160cde5d2
A.A.M.S <aliaikmohammedshameem@gmail.com>
2025-10-22 06:21:39 +0500

    Fixing process function with a code refactor to improve neatness and added documentation to it, to help develpment

--- main.py
+++ main.py
@@ -5,24 +5,54 @@ from tkinter import ttk
 root = tk.Tk() # Declare the root object
 
 def process():
-    output = [0 for _ in range(10)]
-    count = 0
-    num_subjects = 0
-    total_mark = 0
-    for element in entries.subjects:
-        output[count] = float(entries.subjects[element].get())
-        if output[count] != -1.0:
-            num_subjects += 1
-            total_mark += output[count]
-        count += 1
-    agregate = 0
-    if num_subjects != 0:
-        agregate = (total_mark / (100 * num_subjects)) * 100
+    """
+    Purpose
+    --------
+    To handle what pressing the button to process does.
+    Retreves all the enterd marks of the user, and outputs them in the aggregate mark entry
     
-    print(output)
-    print(agregate)
+    Variables
+    ----------
+    count - a counter variable
+    num_subjects - counter for the number of subjects data is input for
+    total_mark - used for totaling the mark of the user, to help with calculating aggregate marks
+    agregate - holds the average marks of the user
+    
+    Lists
+    ------
+    output - list storing marks of the user per subject
+    
+    Global vars used
+    -----------------
+    aggregate_marks - a dictionary which is the aggregate_marks entry showing the answer of the calculation to the user
+    entries.subjects = the dictionary that has the entries that user inputs their mark to
+    """
+    # Initialising Lists
+    retrived_marks = [0 for _ in range(10)]
+    # Initialising Variables
+    subject_no = 0
+    number_subjects_processed = 0
+    total_of_all_marks = 0
+    aggregate_marks = 0
+    # Loop to retreve, store and total users marks and find the number of subjects user takes
+    for element in entries.subjects:
+        retrived_marks[subject_no] = float(entries.subjects[element].get())
+        if retrived_marks[subject_no] != -1.0:
+            number_subjects_processed += 1
+            total_of_all_marks += retrived_marks[subject_no]
+        subject_no += 1
+    # Checks if the user did input data fr any subject and finds aggregate marks
+    # avoids ZeroDivisionError
+    # Outputs as a percentage of 100
+    if number_subjects_processed != 0:
+        aggregate_marks = (total_of_all_marks / (100 * number_subjects_processed)) * 100
+    # Print to console as debug logs
+    print(f'{number_subjects_processed} subjects detected')
+    print(f'Users retreved marks are {retrived_marks}')
+    print(f'Users aggregate marks (after calculation){aggregate_marks}')
+    #  Display marks in the aggregate marks entry
     aggregate_marks['entry'].delete(0, 'end')
-    aggregate_marks['entry'].insert(0, agregate)
+    aggregate_marks['entry'].insert(0, aggregate_marks)
 
 
 root.title('Marks aggregator v1.0') # makes the title of the window
